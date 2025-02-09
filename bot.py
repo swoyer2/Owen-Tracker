0ba39076beab823c52dca544a4263828ec7c62ae
faithmiller299 <43830142+swoyer2@users.noreply.github.com>
2025-02-09 00:10:45 -0500

    added $graph

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import threading
 import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import config
 
@@ -52,16 +53,40 @@ async def on_message(message):
 	if message.content.startswith('$status'):
 		member_name_to_check = 'owowowen'
 		if "user=" in message.content:
-			_, _, user = message.content.partition('user=')
-			member_name_to_check = user
+			_, _, user_name = message.content.partition('user=')
+			member_name_to_check = user_name
 		for member in client.get_all_members():
 			if str(member.name) == member_name_to_check:
 				online_statuses = df[(df['user_name'] == member.name) & (df['status'] != 'offline')]
+				offline_statuses = df[(df['user_name'] == member.name) & (df['status'] == 'offline')]
 				try:
 					last_online = online_statuses.iloc[-1]['time']
+					last_offline = offline_statuses.iloc[-1]['time']
 				except:
-					last_online = 'Never seen online'
+					last_online = 'Never'
+					last_offline = 'Never'
 				member_status = member.raw_status
-				await message.channel.send(f'{member} is {member_status}. Last online at {last_online}.')
+
+				if member_status != 'offline':
+					await message.channel.send(f'{member} is {member_status} since {last_online}')
+				else:
+					await message.channel.send(f'{member} is {member_status}.\nLast online from {last_online} till {last_offline}.')
+	
+	if message.content.startswith('$graph'):
+		user_name = 'owowowen'
+		if "user=" in message.content:
+			_, _, user_name = message.content.partition('user=')
+
+		times = df[(df['user_name'] == user_name)]['time'].values
+		statuses = df[(df['user_name'] == user_name)]['status'].apply(lambda x: 1 if x == 'online' else 0).values
+		plt.step(times, statuses, where='post', label='Online/Offline', color='b')
+		plt.yticks([0, 1], ['Offline', 'Online'])
+		plt.xlabel('Time')
+		plt.ylabel('Status')
+		plt.title(f'{user_name} Status Over Time')
+		plt.xticks(rotation=45)
+		plt.savefig('user_status_over_time.png', bbox_inches='tight')
+		await message.channel.send(file=discord.File('user_status_over_time.png'))
+		
 
 client.run(config.api_key, log_handler=config.handler)
